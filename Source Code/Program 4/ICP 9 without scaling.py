d573e619eb484d57a4c5cf4018d785c8fde9cf8d
devnac221990 <devnac221990@users.noreply.github.com>
2020-07-09 16:26:47 -0500

    Deep Learning ICP 9

---
+++ Source Code/Program 4/ICP 9 without scaling.py
@@ -0,0 +1,56 @@
+from keras import Sequential
+from keras.datasets import mnist
+import numpy as np
+from keras.layers import Dense
+from keras.utils import to_categorical
+from tensorflow.keras import layers
+import matplotlib.pyplot as plt
+
+
+
+(train_images,train_labels),(test_images, test_labels) = mnist.load_data()
+
+print(train_images.shape[1:])
+#process the data
+#1. convert each image of shape 28*28 to 784 dimensional which will be fed to the network as a single feature
+dimData = np.prod(train_images.shape[1:])
+print(dimData)
+train_data = train_images.reshape(train_images.shape[0],dimData)
+test_data = test_images.reshape(test_images.shape[0],dimData)
+
+#convert data to float and scale values between 0 and 1
+train_data = train_data.astype('float')
+test_data = test_data.astype('float')
+
+#change the labels frominteger to one-hot encoding. to_categorical is doing the same thing as LabelEncoder()
+train_labels_one_hot = to_categorical(train_labels)
+test_labels_one_hot = to_categorical(test_labels)
+
+#creating network
+model = Sequential()
+model.add(Dense(512, activation='tanh', input_shape=(dimData,)))
+model.add(Dense(512, activation='tanh'))
+model.add(Dense(512, activation='tanh'))
+model.add(Dense(512, activation='tanh'))
+model.add(Dense(10, activation='softmax'))
+
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+history = model.fit(train_data, train_labels_one_hot, batch_size=256, epochs=10, verbose=1,
+                   validation_data=(test_data, test_labels_one_hot))
+
+plt.subplot(1, 2, 1)
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label='val_accuracy')
+plt.xlabel("Epochs")
+plt.ylabel("Accuracy")
+plt.legend(loc='lower right')
+
+plt.subplot(1, 2, 2)
+plt.plot(history.history['loss'], label='loss')
+plt.plot(history.history['val_loss'], label='val_loss')
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+plt.legend(loc='lower right')
+
+plt.tight_layout()
+plt.show()
